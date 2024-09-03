0ea796662c5048574bafbf24ea5bd3508298934f
Calebe-Dve <calebe.araujo101@gmail.com>
2024-09-03 08:09:23 -0300

    Acrecentando mais itens a lista

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,10 +9,28 @@ import { product } from '../product';
 export class ProductComponent {
     products : product[] = [
       {
-        "id"        :1,
+        "id"        :1, 
         "name"      :"Computador i7",
         "price"     :6000.00,
-        "category"  :"Eletronicos"
+        "category"  :"Eletronicos",
+      },
+      {
+        "id"        :2, 
+        "name"      :"Computador i5",
+        "price"     :4000.00,
+        "category"  :"Eletronicos",
+      },
+      {
+        "id"        :1, 
+        "name"      :"Computador i3",
+        "price"     :1800.00,
+        "category"  :"Eletronicos",
+      },
+      {
+        "id"        :1, 
+        "name"      :"Computador Xeon",
+        "price"     :2000.00,
+        "category"  :"Eletronicos",
       }
     ];
 }
